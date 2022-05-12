38d525698eee23ff5178fcf8c2549ad653d27942
meehog527 <52838950+meehog527@users.noreply.github.com>
2022-05-12 16:22:57 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 echo "Installing python packages"
-pip3 install bluepy
-pip3 install -r requirements.txt
+sudo pip3 install bluepy
+sudo pip3 install -r requirements.txt
+sudo apt-get remove git -y
+sudo apt-get clean
